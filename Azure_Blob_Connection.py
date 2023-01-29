c41b4347023f55ab33df03216e81629392e1e087
Mikerosoftpro <bean.michael2528@gmail.com>
2023-01-29 08:29:44 -0500

    Create Azure_Blob_Connection.py

---
+++ Azure_Blob_Connection.py
@@ -0,0 +1,14 @@
+from azure.storage.blob import BlobServiceClient
+
+# Create a blob service client
+blob_service_client = BlobServiceClient.from_connection_string(
+    'yourconnectionstring')
+
+# Get a container client
+container_client = blob_service_client.get_container_client('yourcontainer')
+
+# Get a blob client
+blob_client = container_client.get_blob_client('yourblob')
+
+# Download the blob
+blob_client.download_blob().readall()
