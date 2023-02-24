0d9a5d43f5b925eda0f44da4d345b78cd61027e7
AnalystBean <121896542+AnalystBean@users.noreply.github.com>
2023-02-24 17:13:30 -0500

    Update S3_Query_AWS_Example.py

--- S3_Query_AWS_Example.py
+++ S3_Query_AWS_Example.py
@@ -1,9 +1,9 @@
 from google.cloud import storage
 
-# Create a client object
+
 client = storage.Client()
 
-# List all the buckets in your project
+
 buckets = list(client.list_buckets())
 print("Buckets:")
 for bucket in buckets:
