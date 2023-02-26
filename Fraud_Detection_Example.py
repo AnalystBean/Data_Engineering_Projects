1713cef8b8d046cfe6cca92588a6db98ace492d4
AnalystBean <121896542+AnalystBean@users.noreply.github.com>
2023-02-25 21:32:36 -0500

    Update Fraud_Detection_Example.py

--- Fraud_Detection_Example.py
+++ Fraud_Detection_Example.py
@@ -4,18 +4,17 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 
-# Load the dataset
+
 df = pd.read_csv('creditcard.csv')
 
-# Preprocessing
-# - Split the data into features and labels
+
 X = df.drop('Class', axis=1)
 y = df['Class']
 
-# - Split the data into training and testing sets
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# - Scale the features
+
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
